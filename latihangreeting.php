a34d3a5433278698e09f1f21c6e8d9c1a4b1ca1f
willian7 <nanang@unitri.ac.id>
2017-04-07 11:00:25 +0700

    echo ?

--- latihangreeting.php
+++ latihangreeting.php
@@ -4,46 +4,24 @@
 </head>
 <body>
 <?php
-
-echo "Fungsi Greeting";
-echo "<br>";
-echo "Istaqo Mahmudah Harits";
-echo "<br>";
-echo "MI 2C";
-echo "<br>";
-echo "1431143021";
-echo "<br>";
-echo "===================";
-echo "<br>";
+echo "Fungsi Greeting<br>Istaqo Mahmudah Harits<br>MI 2C<br>1431143021<br>===================<br>";
 date_default_timezone_set("Asia/Jakarta");
-echo "<br>";
 $a = date("D/M/j \ H:i:s");
-	echo $a;
-echo "<br>";
-
+echo $a;
 $gret = date("G");
-
-echo "<br>";
-echo "===================";
-echo "<br>";
-
-if ($gret>=0 && $gret<10)
-	{
+echo "<br>===================<br>";
+if ($gret>=0 && $gret<10){
 		echo "Selamat Pagi ! :)";
 	}
-elseif ($gret>=10 && $gret<12)
-	{
+elseif ($gret>=10 && $gret<12){
 		echo "Selamat Siang ! :) ";
 	}
-elseif ($gret>=12 && $gret<18)
-	{
+elseif ($gret>=12 && $gret<18){
 		echo "Selamat Sore ! :) ";
 	}
-else
-	{
+else{
 		echo "Selamat Malam ! :)";
 	}
-
 ?>
 </body>
 </html>
